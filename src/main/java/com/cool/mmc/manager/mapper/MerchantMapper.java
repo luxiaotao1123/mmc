4a16b5bd8d852ab7f34ebad948cd17ea5007dcdf
vincent <1341870251@qq.com>
2020-01-02 19:12:25 +0800

    [update]支付模块

--- src/main/java/com/cool/mmc/manager/mapper/MerchantMapper.java
+++ src/main/java/com/cool/mmc/manager/mapper/MerchantMapper.java
@@ -1,17 +1,15 @@
 package com.cool.mmc.manager.mapper;
 
-import com.cool.mmc.manager.entity.Merchant;
 import com.baomidou.mybatisplus.mapper.BaseMapper;
+import com.cool.mmc.manager.entity.Merchant;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Repository;
 
 @Mapper
 @Repository
 public interface MerchantMapper extends BaseMapper<Merchant> {
 
-    @Select("select * from man_merchant where 1=1 and product_id=2 order by id desc limit 0,1")
     Merchant poll(@Param("productId")Long productId);
 
 }
