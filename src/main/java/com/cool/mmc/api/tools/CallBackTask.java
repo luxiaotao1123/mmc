4ffed984b2f9282809e375c995c465d32c8114ed
vincent <1341870251@qq.com>
2020-01-03 18:55:11 +0800

    [update]支付模块

--- src/main/java/com/cool/mmc/api/tools/CallBackTask.java
+++ src/main/java/com/cool/mmc/api/tools/CallBackTask.java
@@ -2,7 +2,6 @@ package com.cool.mmc.api.tools;
 
 import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
-import com.cool.mmc.manager.entity.Oauth;
 import com.cool.mmc.manager.entity.Timer;
 import com.cool.mmc.manager.service.TimerService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +9,8 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
+import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 
 /**
@@ -31,9 +29,10 @@ public class CallBackTask {
         for(Timer timer:timers){
             String post = HttpSend.doPost(timer.getUrl(), JSONObject.parseObject(timer.getData()));
             JSONObject jsonObject = JSONObject.parseObject(post);
-            if(jsonObject.getString("code")=="200"){
+            if(jsonObject.getString("code").equals("200")){
                 timer.setStatus(1);
             }
+            timer.setUpdateTime(new Date());
             timer.setCount(timer.getCount()+1);
             timerService.updateById(timer);
         }
